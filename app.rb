7c8a22f54df57b386e6ae18bef16d6c2d0f535f8
ehsan <ehsan.khf@gmail.com>
2017-07-09 07:15:46 +0000

    reveal word

--- app.rb
+++ app.rb
@@ -40,7 +40,7 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    if (@game.guesses.include? :guess) || (@game.guesses.include? :guess)
+    if (@game.guesses.include? letter) || (@game.guesses.include? letter)
       flash[:message] = "You have already used that letter."
     end
     
